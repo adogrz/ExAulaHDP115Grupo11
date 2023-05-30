c28e8ac1c9f8cfab01cdce3c93f4fe37461b6cc6
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-05-30 01:28:49 -0600

    Agregado login para usuario

--- InflacionCanastaBasica/urls.py
+++ InflacionCanastaBasica/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("", views.precio_anual, name="precio_anual"),
     path("signup/", views.signup, name="signup"),
     path("signout/", views.signout, name="signout"),
+    path("signin/", views.signin, name="signin"),
 ]
--- canasta/views.py
+++ canasta/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
-from django.contrib.auth import login, logout
+from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
 
 # Create your views here.
@@ -37,3 +37,19 @@ def signup(request):
 def signout(request):
     logout(request)
     return redirect('precio_anual')
+
+def signin(request):
+    if request.method == 'GET':
+        return render(request, 'signin.html', {
+            'form': AuthenticationForm
+        })
+    else:
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request, 'signin.html', {
+                'form': AuthenticationForm,
+                'error': 'Username or password did not match'
+            })
+        else:
+            login(request, user)
+            return redirect('precio_anual')
