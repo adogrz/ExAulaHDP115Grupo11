5e3412165bbcf04e65df3cd37554ee8721ded1c0
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-06-03 17:44:44 -0600

    Reparado comando create_custom_superuser en build.sh

--- build.sh
+++ build.sh
@@ -7,4 +7,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
 python manage.py import_prices_CB_from_csv canasta/data/anios.csv canasta/data/precios.csv
-python manage.py createcustomsuperuser admin admin@admin.com --password admin
+python manage.py create_custom_superuser admin admin@admin.com --password admin
