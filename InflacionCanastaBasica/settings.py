136694aae8415f926bf3530456a7cd92fbfa1ff1
Adonay Gutierrez <adonaygutierrez50@gmail.com>
2023-06-03 20:54:18 -0600

    Agregada settings para despliegue

--- InflacionCanastaBasica/settings.py
+++ InflacionCanastaBasica/settings.py
@@ -84,7 +84,11 @@ WSGI_APPLICATION = "InflacionCanastaBasica.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(default="sqlite:///db.sqlite3")
+    'default': dj_database_url.config(
+        # Feel free to alter this value to suit your needs.
+        default='postgresql://postgres:postgres@localhost:5432/postgres',
+        conn_max_age=600
+    )
 }
 
 # Password validation
